aee1ccab3fc3fa48a3c42c8b3338120f3947fa6a
santi_s <santsasa@gmail.com>
2017-01-31 17:49:16 -0600

    Added comments and Open multiannual time series The script now can open 'Fremont.FLOWS.2011.2015.txt' which will be used to identify potential spawning events based on flow at Fremont

--- spawning_events_analysis.m
+++ spawning_events_analysis.m
@@ -1,4 +1,4 @@
-clear
+%% Open spawning event time series
 fid             = fopen('spawning.events.2015.txt');
 data.title      = textscan(fid, '%s', 1, 'Delimiter', '\n');
 data.sourcefile = textscan(fid, '%s', 1, 'Delimiter', '\n');
@@ -6,10 +6,22 @@ data.header     = textscan(fid, '%s', 3, 'Delimiter', '\t');
 data.sample     = textscan(fid, '%f %s %f','Delimiter', '\t');
 fclose(fid)
 
+% Vectorize data
 sampleN = data.sample{1};
 time    = data.sample{2};
 values  = data.sample{3};
 
+% Find lowest discharge from spawning events
 indx = find(values == min(values));
-minQ = values(indx);
-time_minQ = time{indx};
+minQ = values(indx);    % Lowest discharge at Fremont
+time_minQ = time{indx}; % Time of lowest discharge
+
+%% Open multiannual timeseries
+fid        = fopen('Fremont.FLOWS.2011.2015.txt');
+dss.path   = textscan(fid, '%s', 1, 'Delimiter', '\n');
+dss.header  = textscan(fid, '%s', 3, 'Delimiter', '\t');
+textscan(fid,'%*s \n')
+dss.units  = textscan(fid, '%*s %*s %s', 1, 'Delimiter', '\t');
+dss.type   = textscan(fid, '%*s %*s %s', 1, 'Delimiter', '\t');
+dss.data   = textscan(fid, '%f %s %f','Delimiter', '\t');
+fclose(fid)
