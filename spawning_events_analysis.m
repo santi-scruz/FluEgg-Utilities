750366b299afddc9f716fb5bc4496a52115bd45e
santi_s <santsasa@gmail.com>
2017-02-07 14:44:17 -0600

    Compute and Export Rise and Descending limb times

--- spawning_events_analysis.m
+++ spawning_events_analysis.m
@@ -29,6 +29,7 @@ fclose(fid);
 %Date formatting
 date_time = dss.data{1,2};
 T = length(date_time); %To be deleted in future ONLY FOR TEST PURPOSES
+T = 4000;
 dats = cellfun(@(x) parse_date(x), date_time(1:T));
 values = dss.data{1,3};
 
@@ -55,7 +56,16 @@ highflow.event.maxValue_indx  = arrayfun(@(x,y,z) find(values(x:y) == z, 1),...
                                    highflow.event.maxValue);
 highflow.event.maxValue_time  = arrayfun(@(x) dats(x),...
                 (highflow.event.maxValue_indx + highflow.event.start_indx));
-
+            
+%% Raising and descending times
+% Computation of the times from start of event to peak, and peak time to
+% end of event.
+highflow.event.risetime = arrayfun(@(x,y) round((y - x)*24*60,2),...
+                          highflow.event.start_time,...
+                          highflow.event.maxValue_time);         
+highflow.event.descendtime = arrayfun(@(x,y) round((y - x)*24*60,2),...
+                          highflow.event.maxValue_time,...
+                          highflow.event.end_time); 
 %% Plotting
 figure
 plot(dats, values(1:T))
@@ -68,12 +78,17 @@ datetick('x',12)
 ylabel('Flow (cms)')
 grid off
 
+figure
+
 %% Export to Table
 id = (1:length( highflow.event.maxValue))';
 t = table(id, ...
           datestr(highflow.event.start_time),...
           datestr(highflow.event.end_time),...
           datestr(highflow.event.maxValue_time),...
-          highflow.event.maxValue);
-t.Properties.VariableNames = {'id' 'StartTime' 'EndTime' 'PeakTime' 'PeakFlow'};
-writetable(t,'Summary.txt','Delimiter','\t')
+          highflow.event.maxValue,...
+          highflow.event.risetime,...
+          highflow.event.descendtime);
+t.Properties.VariableNames = {'id' 'Start' 'End' 'PeakTime' ...
+    'PeakFlow_cms' 'RiseTime_min' 'DescendTime_min'};
+writetable(t,'Summary2.txt','Delimiter','\t')
